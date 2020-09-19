a96d330bbcd416b272530d4c5d19698f6cd4434c
andy <r960411@.gmail.com>
2020-09-19 13:39:20 +0800

    Not very good

--- get_pcd/src/get_pointcloud.cpp
+++ get_pcd/src/get_pointcloud.cpp
@@ -7,15 +7,18 @@
 #include <pcl/point_types.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/voxel_grid.h>
+#include "pcl_ros/transforms.h"
+
 #include <iostream>
 #include <string>
-#include "get_pcd/save_pcd.h"
-
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
-
 #include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
 #include <yaml-cpp/yaml.h>
+#include "get_pcd/save_pcd.h"
+#include "realsense2_camera/Extrinsics.h"
+
 #define PCDSONCE 5 // PCDSONCE pcd per service call
 using namespace std;
 
@@ -24,14 +27,33 @@ typedef pcl::PointXYZRGB PointTRGB;
 
 std::string save_path_cloud = ros::package::getPath("get_pcd") + "/data/save_cloud/";       // [5] cloud_rs; [6] all_rs
 std::string file_name = "";
-bool do_save = false;
+bool save_mix = false;
 
 YAML::Node doc;
 boost::thread* save_file_thread;
+boost::thread* pcd_sum_thread;
+boost::mutex mutex;
 string *file_path;
-Eigen::MatrixXd curr_trans = Eigen::MatrixXd::Identity(4,4);
+Eigen::MatrixXf curr_trans = Eigen::MatrixXf::Identity(4,4);
+Eigen::MatrixXf d2c_trans = Eigen::MatrixXf::Identity(4,4);
+
+pcl::PointCloud<PointTRGB>::Ptr cloud_mix(new pcl::PointCloud<PointTRGB>);
 
 int cnt = PCDSONCE * 10;
+int mix_cnt = 0;
+
+void set_d2color_trans(const realsense2_camera::Extrinsics &trans)
+{
+  for(int i=0; i<3; i++)
+  {
+    for(int j=0; j<3; j++)
+    {
+      d2c_trans(i, j) = trans.rotation[i*3+j];
+    }
+    d2c_trans(i, 3) = trans.translation[i];
+  }
+  cout << d2c_trans << endl;
+}
 
 void save_pcd(pcl::PointCloud<PointTRGB>::Ptr pcd, string* path)
 {
@@ -40,34 +62,74 @@ void save_pcd(pcl::PointCloud<PointTRGB>::Ptr pcd, string* path)
   return;
 }
 
+void add_two_pcd(pcl::PointCloud<PointTRGB>::Ptr a, pcl::PointCloud<PointTRGB>::Ptr b)
+{
+  mutex.lock();
+  *a = *a + *b;
+  mutex.unlock();
+}
+
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-  if(cnt > PCDSONCE * 3)  
-    return;
-  pcl::PointCloud<PointTRGB>::Ptr cloud(new pcl::PointCloud<PointTRGB>);  
-  pcl::fromROSMsg(*cloud_msg, *cloud);
-  if(cnt%3 == 0) //save one pcd every three times
+  if(cnt <= PCDSONCE * 3 && cnt%3 == 0)  //save one pcd every three times
   {
+    pcl::PointCloud<PointTRGB>::Ptr cloud(new pcl::PointCloud<PointTRGB>);
+    pcl::PointCloud<PointTRGB>::Ptr cloud_base(new pcl::PointCloud<PointTRGB>);
+    pcl::fromROSMsg(*cloud_msg, *cloud);
+    Eigen::Matrix4f matrix = curr_trans * d2c_trans;
+    pcl::transformPointCloud(*cloud, *cloud_base, matrix);
+
+    if(cloud_mix->empty())
+    {
+      *cloud_mix =  *cloud_base;
+    }
+    else
+    {
+      pcd_sum_thread = new boost::thread(boost::bind(&add_two_pcd, cloud_mix, cloud_base));
+      // delete pcd_sum_thread;
+    }
+    file_path = new string(save_path_cloud + file_name + std::to_string(cnt) + ".pcd");
     // ========go thread========
-    file_path = new string;
-    *file_path = save_path_cloud + file_name + std::to_string(cnt) + ".pcd";
-    save_file_thread = new boost::thread(boost::bind(&save_pcd, cloud, file_path));
+    save_file_thread = new boost::thread(boost::bind(&save_pcd, cloud_base, file_path));
     delete save_file_thread;
     // =======no thread=======
-    // string file_path_cloud = save_path_cloud + file_name + std::to_string(cnt) + ".pcd";
-    // cnt ++;
-    // pcl::io::savePCDFileASCII<PointTRGB>(file_path_cloud, *cloud);
-    // cout << "Cloud saved: " << file_path_cloud << "; (width, height) = " << cloud->width << ", " << cloud->height << endl;
+    // pcl::io::savePCDFileASCII<PointTRGB>(*file_path, *cloud);
+    // cout << "Cloud saved: " << *file_path << "; (width, height) = " << cloud->width << ", " << cloud->height << endl;
+  }
+  else if(save_mix)
+  {
+    ros::Rate rate(10);
+    while(!pcd_sum_thread->try_join_for(boost::chrono::microseconds(10)))
+      rate.sleep();
+
+    string file_path_mix = string(save_path_cloud + "cloud_mix_" + std::to_string(mix_cnt) + ".pcd");
+    pcl::io::savePCDFileASCII<PointTRGB>(file_path_mix, *cloud_mix);
+    cout << "Cloud saved: " << file_path_mix << "; (width, height) = " << cloud_mix->width << ", " << cloud_mix->height << endl;
+    mix_cnt++;
+    save_mix = false;
   }
   cnt++;
+  return;
 }
 
 bool get_pcd_callback (get_pcd::save_pcd::Request &req, get_pcd::save_pcd::Response &res)
 {
   file_name = req.name;
-  do_save = true;
-  res.done = true;
+  save_mix = req.save_mix;
   cnt = 0;
+  
+  if(!req.curr_trans.empty())
+  {
+    for(int i=0; i<3; i++)
+    {
+      for(int j=0; j<3; j++)
+      {
+        curr_trans(i, j) = req.curr_trans[i*3+j];
+      }
+      curr_trans(i, 3) = req.curr_trans[i];
+    }
+  }
+  res.done = true;
   return true;
 }
 
@@ -77,8 +139,9 @@ int main (int argc, char** argv)
   ros::init (argc, argv, "my_pcl_tutorial");
   ros::NodeHandle n;
   // Create a ROS subscriber for the input point cloud
+  ros::Subscriber d_to_color_sub = n.subscribe("/camera/extrinsics/depth_to_color", 1, &set_d2color_trans);
   ros::Subscriber point_sub = n.subscribe("/camera/depth_registered/points", 1, &cloud_cb);
-  ros::ServiceServer service = n.advertiseService("/get_pcd", &get_pcd_callback);
+  ros::ServiceServer get_pc_service = n.advertiseService("/get_pcd", &get_pcd_callback);
   ros::spin ();
 }
 
