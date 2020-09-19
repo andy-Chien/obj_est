0ee11fd48f8237bc4c2142696a74aab480c1c91d
andy <r960411@.gmail.com>
2020-09-19 09:30:33 +0800

    Add Thread to save pcd

--- get_pcd/src/get_pointcloud.cpp
+++ get_pcd/src/get_pointcloud.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -6,84 +7,81 @@
 #include <pcl/point_types.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/voxel_grid.h>
-
 #include <iostream>
+#include <string>
+#include "get_pcd/save_pcd.h"
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
+#include <boost/thread.hpp>
+#include <yaml-cpp/yaml.h>
+#define PCDSONCE 5 // PCDSONCE pcd per service call
+using namespace std;
 
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointXYZRGB PointTRGB;
 
-std::string open_path_image = "../test_pcl_ws/src/pcl_test/data/save_img/test_img.jpg";   // [1] img_file
-std::string save_path_rgb = "../test_pcl_ws/src/pcl_test/data/save_img/rgb_";             // [2] img_rs_rgb; [6] all_rs
-std::string save_path_depth = "../test_pcl_ws/src/pcl_test/data/save_img/depth_";         // [3] img_rs_depth; [6] all_rs
-std::string open_path_cloud = "../test_pcl_ws/src/pcl_test/data/save_cloud/test_cloud.pcd";// [4] cloud_file
-std::string save_path_cloud = "../test_pcl_ws/src/pcl_test/data/save_cloud/cloud_";       // [5] cloud_rs; [6] all_rs
+std::string save_path_cloud = ros::package::getPath("get_pcd") + "/data/save_cloud/";       // [5] cloud_rs; [6] all_rs
+std::string file_name = "";
+bool do_save = false;
 
-int cnt_rgb = 0;
-int cnt_depth = 0;
-int cnt_cloud = 0;
+YAML::Node doc;
+boost::thread* save_file_thread;
+string *file_path;
+Eigen::MatrixXd curr_trans = Eigen::MatrixXd::Identity(4,4);
 
-void image_cb_rgb(const sensor_msgs::ImageConstPtr &msg);
-void image_cb_depth(const sensor_msgs::ImageConstPtr &msg);
-void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+int cnt = PCDSONCE * 10;
 
-ros::Publisher pub;
-
-using namespace std;
-pcl::PointCloud<PointTRGB>::Ptr cloud(new pcl::PointCloud<PointTRGB>);      //pcl::PointXYZRGB
+void save_pcd(pcl::PointCloud<PointTRGB>::Ptr pcd, string* path)
+{
+  pcl::io::savePCDFileASCII<PointTRGB>(*path, *pcd);
+  cout << "Cloud saved: " << *path << "; (width, height) = " << pcd->width << ", " << pcd->height << endl;
+  return;
+}
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-  // // Container for original & filtered data
-  // pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-  // pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-  // pcl::PCLPointCloud2 cloud_filtered;
-
-  // // Convert to PCL data type
-  // pcl_conversions::toPCL(*cloud_msg, *cloud);
-
-  // // Perform the actual filtering
-  // pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-  // sor.setInputCloud (cloudPtr);
-  // sor.setLeafSize (0.1, 0.1, 0.1);
-  // sor.filter (cloud_filtered);
-
-  // // Convert to ROS data type
-  // sensor_msgs::PointCloud2 output;
-  // pcl_conversions::moveFromPCL(cloud_filtered, output);
-
-  // // Publish the data
-  // pub.publish (output);
-
-    // 將點雲格式由sensor_msgs/PointCloud2轉成pcl/PointCloud(PointXYZ, PointXYZRGB)
+  if(cnt > PCDSONCE * 3)  
+    return;
+  pcl::PointCloud<PointTRGB>::Ptr cloud(new pcl::PointCloud<PointTRGB>);  
   pcl::fromROSMsg(*cloud_msg, *cloud);
-
-  // Save PointCloud 
-  ostringstream os;
-  os << cnt_cloud;
-  string file_path_cloud = save_path_cloud + os.str();
-  file_path_cloud = file_path_cloud + ".pcd";
-  
-  pcl::io::savePCDFileASCII<PointTRGB>(file_path_cloud, *cloud);
-  cout << "Cloud saved: " << file_path_cloud << "; (width, height) = " << cloud->width << ", " << cloud->height << endl;
-
-  cnt_cloud++;
+  if(cnt%3 == 0) //save one pcd every three times
+  {
+    // ========go thread========
+    file_path = new string;
+    *file_path = save_path_cloud + file_name + std::to_string(cnt) + ".pcd";
+    save_file_thread = new boost::thread(boost::bind(&save_pcd, cloud, file_path));
+    delete save_file_thread;
+    // =======no thread=======
+    // string file_path_cloud = save_path_cloud + file_name + std::to_string(cnt) + ".pcd";
+    // cnt ++;
+    // pcl::io::savePCDFileASCII<PointTRGB>(file_path_cloud, *cloud);
+    // cout << "Cloud saved: " << file_path_cloud << "; (width, height) = " << cloud->width << ", " << cloud->height << endl;
+  }
+  cnt++;
 }
 
+bool get_pcd_callback (get_pcd::save_pcd::Request &req, get_pcd::save_pcd::Response &res)
+{
+  file_name = req.name;
+  do_save = true;
+  res.done = true;
+  cnt = 0;
+  return true;
+}
 
 int main (int argc, char** argv)
 {
-  
   // Initialize ROS
   ros::init (argc, argv, "my_pcl_tutorial");
-  ros::NodeHandle nh;
   ros::NodeHandle n;
-
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/camera/depth/color/points", 1, cloud_cb);
-
-  // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("/process_cloud", 1);
-  
-  // Spin
+  ros::Subscriber point_sub = n.subscribe("/camera/depth_registered/points", 1, &cloud_cb);
+  ros::ServiceServer service = n.advertiseService("/get_pcd", &get_pcd_callback);
   ros::spin ();
 }
+
+// *a =*a +*b;
+// Eigen::Matrix4f matrix =Eigen::Matrix4f::Identity();
+// pcl::transformPointCloud(*a, *a_trans, matrix);
